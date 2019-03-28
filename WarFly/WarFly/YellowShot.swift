fcc0885130ed52cae55be2c785f66a43cd5b2c95
Anastasiia <anastasia.halk27@gmail.com>
2019-03-28 15:23:43 +0300

    player Fire

---
+++ WarFly/WarFly/YellowShot.swift
@@ -0,0 +1,21 @@
+//
+//  YellowShot.swift
+//  WarFly
+//
+//  Created by Admin on 28/03/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import SpriteKit
+
+class YellowShot: Shot {
+    
+    init() {
+        let textureAtlas =  SKTextureAtlas(named: "YellowShot")
+        super.init(textureAtlas: textureAtlas)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
